22707c437f2fb897e32ad27697b97fc237360b88
RaulFernandezRuiz <73242674+RaulFernandezRuiz@users.noreply.github.com>
2020-11-19 13:42:17 +0100

    Balas y plataformas que se mueven
    Se ha implementado una función en la que solo tenemos que poner la posición en X, posición en Y y sentido. La función se encarga de las colisiones, destruirlas si está fuera del mapa o han chocado.
    
    Respecto a las plataformas se han creado una función en la que indicamos la posición en X, posición en Y y velocidad. Está función se encarga de meterlas en un grupo y  hacer que el jugador siga el movimiento de la plataforma si esta sobre ella.

--- src/js/demo.js
+++ src/js/demo.js
@@ -10,6 +10,8 @@ class DemoScene extends Phaser.Scene
         this.load.image('sky','../resources/img/sky.png');
         this.load.image('platform','../resources/img/platform.png');
 
+        this.load.image('bomb','../resources/img/bomb.png');// ESTE ASSET SERA CAMBIADO POR UNA BALA AHORA SE USA DE TESTEO
+
         this.load.spritesheet('dude','../resources/img/dude.png',
         { frameWidth:32, frameHeight: 48});
 
@@ -23,10 +25,9 @@ class DemoScene extends Phaser.Scene
         this.platforms = this.physics.add.staticGroup();
         this.platforms.create(400,568,'platform').setScale(2).refreshBody();
         this.platforms.create(300,450,'platform');
-        this.platforms.create(450,350,'platform');
-        this.platforms.create(300,-100,'platform');
-        
-        this.player1 = this.physics.add.sprite(100,450,'dude');
+       
+
+        this.player1 = this.physics.add.sprite(40,450,'dude');
         this.player1.setBounce(0);
         this.player1.setCollideWorldBounds(true);
 
@@ -36,21 +37,91 @@ class DemoScene extends Phaser.Scene
 
         this.createAnimations();
 
-        this.colP1Plat = this.physics.add.collider(this.player1, this.platforms, this.allowJump1,null,this);
-        this.colP2Plat = this.physics.add.collider(this.player2, this.platforms, this.allowJump2,null,this);
+        this.colP1Plat = this.physics.add.collider(this.player1, this.platforms);
+        this.colP2Plat = this.physics.add.collider(this.player2, this.platforms);
 
         this.player1Controls = this.input.keyboard.addKeys('W,A,D');
         this.player2Controls = this.input.keyboard.addKeys('UP,LEFT,RIGHT');
+
+        this.grupo_balas= this.add.group();// Este grupo lo usare para recorrer todas mis balas de la escena, IMPORTANTE NO LE PONGO FÍSICAS AL GRUPO
+       
+        this.golpeado=false;
+        this.saberPorQueLadoLeHanGolpeado=0;// 0 izq y 1 derecha
+        this.tiempo=0;
+        this.fin=0;
+
+        this.golpeado2=false;
+        this.saberPorQueLadoLeHanGolpeado2=0;// 0 izq y 1 derecha
+        this.tiempo2=0;
+        this.fin2=0;
+       
+        this.physics.add.overlap(this.grupo_balas,this.player1, this.chocarTrue,null,this);
+        this.physics.add.overlap(this.grupo_balas,this.player2, this.chocarTrue2,null,this);
+        this.generarBalasEnUnSitio(40,400,300);
+        this.MedirCuandoHacerBala=1;
+        this.MedirCuandoHacerBala2=1;  
         
-        //this.camera = this.cameras.main;
-        this.canJump1 = true;
-        this.canJump2 = true;
+        this.grupoPlataformasQueRebotan=this.add.group();//Grupo donde meto todas las plataformas que rebotan
+
+        this.colll=  this.physics.add.collider(this.grupoPlataformasQueRebotan,this.player1,function(grupo,player){// Variable donde guardo las colisiones de las plataformas
+        // que rebotan con el jugador1 
+        player.body.velocity.x=0;
+        grupo.body.setFrictionX(2);
+        });
+
+    
+        this.coll=  this.physics.add.collider(this.grupoPlataformasQueRebotan,this.player2,function(grupo,player){// Variable donde guardo las colisiones de las plataformas
+        // que rebotan con el jugador2    
+        player.body.velocity.x=0;
+        grupo.body.setFrictionX(2);
+        });
+
+        this.generarPlataformasQueRebotan(450,350,100);
+    }  
+        //  this.camera = this.cameras.main;
+    generarBalasEnUnSitio(possX,possY,sentidoYvelocidad){
+            this.bal=this.physics.add.sprite(possX,possY,"bomb");
+            this.bal.body.setAllowGravity(false);
+            this.bal.setVelocity(sentidoYvelocidad,0);
+            this.grupo_balas.add(this.bal);
     }
 
-    update()
+    generarPlataformasQueRebotan(possX,possY,velocidad){
+        this.plataforma1=this.physics.add.sprite(possX,possY,'platform');
+        this.plataforma1.body.setAllowGravity(false);
+        this.plataforma1.body.setImmovable(true);
+        this.plataforma1.setVelocity(velocidad,0);
+        this.plataforma1.setCollideWorldBounds(true);
+        this.plataforma1.setBounce(1);
+        this.grupoPlataformasQueRebotan.add(this.plataforma1);
+    }
+  
+    destruirBalasFueraDelMapa(gb){// FUNCION QUE DESTRUYE LAS BALAS QUE SE SALEN DEL MAPA PARA NO SOBRECARGAR EL JUEGO
+        for(var j=0;j<gb.getChildren().length;j++){
+            if(gb.getChildren()[j].x>config.width-30||gb.getChildren()[j].x<30){
+                 gb.getChildren()[j].destroy();
+             }
+            }
+        }
+    update(time,delta)
     {
         //Player1 control
-        if(this.player1Controls.A.isDown)
+    if(this.golpeado==true){
+
+         this.tiempo=time;
+         this.fin=this.tiempo+30;
+         this.golpeado=false;
+     }
+        if(this.tiempo!=this.fin){          
+            this.tiempo= this.tiempo+1;
+            if(this.saberPorQueLadoLeHanGolpeado==1){// ME HAN PEGADO POR LA DERECHA
+            this.player1.setVelocityX(-250);
+            }
+            else{// ME HAN PEGADO POR LA IZQUIERDA            
+                this.player1.setVelocityX(250);
+            }
+        }
+         else if(this.player1Controls.A.isDown)
         {
             this.player1.setVelocityX(-160);
             this.player1.anims.play('left',true);
@@ -60,28 +131,46 @@ class DemoScene extends Phaser.Scene
             this.player1.setVelocityX(160);
             this.player1.anims.play('right',true);
         }
-        else
+       else
         {
             this.player1.setVelocityX(0);
             this.player1.anims.play('iddle');
         }
 
-        if(this.player1Controls.W.isDown && /*this.player1.body.touching.down*/this.canJump1)
+        if(this.player1Controls.W.isDown && this.player1.body.touching.down)
         {
             this.player1.setVelocityY(-400);
-            this.canJump1 = false;
         }
 
         if(this.player1.body.velocity.y > 1)//cayendo
         {
             this.colP1Plat.active = true;
+            this.colll.active=true;
+
         }else//saltando
         {
             this.colP1Plat.active = false;            
+            this.colll.active=false;
         }
 
         //Player2 control
-        if(this.player2Controls.LEFT.isDown)
+
+        if(this.golpeado2==true){
+            this.tiempo2=time;
+            this.fin2=this.tiempo2+30;
+            this.golpeado2=false;           
+        }
+        if(this.tiempo2!=this.fin2){          
+            this.tiempo2= this.tiempo2+1;
+            if(this.saberPorQueLadoLeHanGolpeado2==1){// ME HAN PEGADO POR LA DERECHA
+            this.player2.setVelocityX(-250);
+            }
+            else{// ME HAN PEGADO POR LA IZQUIERDA            
+                this.player2.setVelocityX(250);
+            }
+        }
+
+        else if(this.player2Controls.LEFT.isDown)
         {
             this.player2.setVelocityX(-160);
             this.player2.anims.play('left2',true);
@@ -97,23 +186,58 @@ class DemoScene extends Phaser.Scene
             this.player2.anims.play('iddle2');
         }
 
-        if(this.player2Controls.UP.isDown && /*this.player2.body.touching.down*/this.canJump2)
+        if(this.player2Controls.UP.isDown && this.player2.body.touching.down)
         {
             this.player2.setVelocityY(-400);
-            this.canJump2 = false;
         }
 
         if(this.player2.body.velocity.y > 1)//cayendo
         {
             this.colP2Plat.active = true;
+            this.coll.active=true;
         }else//saltando
         {
-            this.colP2Plat.active = false;            
-        }        
-        //this.camera.scrollY-=1;
-
+            this.colP2Plat.active = false;         
+            this.coll.active=false;   
+        }
+        //this.camera.scrollY-=1;  
+     //   console.log(time);
+       if(this.MedirCuandoHacerBala<time){
+        this.generarBalasEnUnSitio(40,400,300);
+        this.MedirCuandoHacerBala=time+1200;
+       }
+       if(this.MedirCuandoHacerBala2<time){
+        this.generarBalasEnUnSitio(700,490,-300);
+        
+        this.MedirCuandoHacerBala2=time+1200;
+       }
+       this.destruirBalasFueraDelMapa(this.grupo_balas);
     }
 
+    chocarTrue(gpp,jug){
+        this.s=gpp.body.x;
+        this.golpeado=true;
+
+        if(this.s<jug.x){           
+            this.saberPorQueLadoLeHanGolpeado=0;
+        }
+       else{
+            this.saberPorQueLadoLeHanGolpeado=1;
+       }
+        gpp.destroy();
+    }
+    chocarTrue2(gpp,jug){
+        this.s=gpp.body.x;
+        this.golpeado2=true;
+
+        if(this.s<jug.x){           
+            this.saberPorQueLadoLeHanGolpeado2=0;
+        }
+       else{
+            this.saberPorQueLadoLeHanGolpeado2=1;
+       }
+        gpp.destroy();
+    }
     createAnimations()
     {
         //Animaciones Player1
@@ -154,14 +278,5 @@ class DemoScene extends Phaser.Scene
             frameRate: 10,
             repeat: -1
             });
-    }
-
-    allowJump1()
-    {
-        this.canJump1 = true;
-    }
-    allowJump2()
-    {
-        this.canJump2 = true;
-    }
+    }   
 }
