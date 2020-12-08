ae51775f20bc4cea502da0aeae3fc1851646b719
jaimejl21 <jjimenezlopez21@gmail.com>
2020-12-08 13:45:00 +0100

    Botón en selección de personajes
    Añadido botón para pasar al juego en la pantalla de selección de personajes.

--- src/js/select.js
+++ src/js/select.js
@@ -12,7 +12,7 @@ class Select extends Phaser.Scene
 
     preload()
     {
-        //this.load.audio('select', '../resources/audio/select.mp3');
+        this.load.audio('select', '../resources/audio/select.mp3');
         
         this.load.image('pepe','../resources/img/Personajes/Pepe the Frog.png');
         this.load.image('trollface','../resources/img/Personajes/Troll Face.png');
@@ -23,43 +23,43 @@ class Select extends Phaser.Scene
     {
         this.next = 0;
         
-        /*this.selectAudio = this.sound.add('select', { loop: true });
+        this.selectAudio = this.sound.add('select', { loop: true });
         this.selectAudio.setVolume(0.05);
-        this.selectAudio.play();*/
+        this.updateAudio();
         
-        this.text = this.add.text(400, 150, 'Press to select character', { fontFamily: 'Berlin Sans FB, "Goudy Bookletter 1911", Times, serif', fontSize: '32px', fill: '#fff' });
+        this.text = this.add.text(400, 100, 'Press to select character', { fontFamily: 'Berlin Sans FB, "Goudy Bookletter 1911", Times, serif', fontSize: '42px', fill: '#fff' });
         this.text.setOrigin(0.5);
         this.text.setColor('#FFFFFF');
 
-        this.text = this.add.text(400, 225, 'Player 1', { fontFamily: 'Berlin Sans FB, "Goudy Bookletter 1911", Times, serif', fontSize: '32px', fill: '#fff' });
+        this.text = this.add.text(400, 175, 'Player 1', { fontFamily: 'Berlin Sans FB, "Goudy Bookletter 1911", Times, serif', fontSize: '32px', fill: '#fff' });
         this.text.setOrigin(0.5);
         this.text.setColor('#FFFFFF');
 
-        this.text = this.add.text(400, 375, 'Player 2', { fontFamily: 'Berlin Sans FB, "Goudy Bookletter 1911", Times, serif', fontSize: '32px', fill: '#fff' });
+        this.text = this.add.text(400, 325, 'Player 2', { fontFamily: 'Berlin Sans FB, "Goudy Bookletter 1911", Times, serif', fontSize: '32px', fill: '#fff' });
         this.text.setOrigin(0.5);
         this.text.setColor('#FFFFFF');
 
-        this.p1 = this.add.image(200, 300, 'pepe').setInteractive()
+        this.p1 = this.add.image(200, 250, 'pepe').setInteractive()
         .on('pointerover', () => this.p1.setScale( 1.2 ))
         .on('pointerout', () => this.p1.setScale( 1 ));
 
-        this.p2 = this.add.image(400, 300, 'trollface').setInteractive()
+        this.p2 = this.add.image(400, 250, 'trollface').setInteractive()
         .on('pointerover', () => this.p2.setScale( 1.2 ))
         .on('pointerout', () => this.p2.setScale( 1 ));
 
-        this.p3 = this.add.image(600, 300, 'coffindancer').setInteractive()
+        this.p3 = this.add.image(600, 250, 'coffindancer').setInteractive()
         .on('pointerover', () => this.p3.setScale( 1.2 ))
         .on('pointerout', () => this.p3.setScale( 1 ));
 
-        this.p4 = this.add.image(200, 450, 'pepe').setInteractive()
+        this.p4 = this.add.image(200, 400, 'pepe').setInteractive()
         .on('pointerover', () => this.p4.setScale( 1.2 ))
         .on('pointerout', () => this.p4.setScale( 1 ));
 
-        this.p5 = this.add.image(400, 450, 'trollface').setInteractive()
+        this.p5 = this.add.image(400, 400, 'trollface').setInteractive()
         .on('pointerover', () => this.p5.setScale( 1.2 ))
         .on('pointerout', () => this.p5.setScale( 1 ));
 
-        this.p6 = this.add.image(600, 450, 'coffindancer').setInteractive()
+        this.p6 = this.add.image(600, 400, 'coffindancer').setInteractive()
         .on('pointerover', () => this.p6.setScale( 1.2 ))
         .on('pointerout', () => this.p6.setScale( 1 ));
 
@@ -73,22 +73,13 @@ class Select extends Phaser.Scene
         this.p4.on('pointerdown', () => this.cambio(this.p4, this.p5, this.p6, 4));
         this.p5.on('pointerdown', () => this.cambio(this.p5, this.p4, this.p6, 5));
         this.p6.on('pointerdown', () => this.cambio(this.p6, this.p4, this.p5, 6));
-        
-        /*this.time.addEvent({
-            delay: 3000,
-            callback: () => { this.scene.start('DemoScene') ;},
-            callbackScope: this
-        });*/
     }
 
     update()
     {
         if (this.next == 2)
         {
-            //this.selectAudio.stop();
-            this.scene.restart('DemoScene');
-            this.scene.start('DemoScene', {eleccion1: this.eleccion1, eleccion2: this.eleccion2});
-
+            this.createButton();
         }
 
     }
@@ -109,6 +100,51 @@ class Select extends Phaser.Scene
             this.eleccion1 = num;
         }
         this.next++;
+    }
+    centerButtonText (gameText, gameButton) {
+        Phaser.Display.Align.In.Center(
+            gameText,
+            gameButton
+        );
+    }
+
+    centerButton (gameObject, offset = 0) {
+        Phaser.Display.Align.In.Center(
+        gameObject,
+        this.add.zone(800/2, 600/2 - offset * 100, 800, 600)
+        );
+    }
+
+    createButton ()
+    {
+        this.nextButton = this.add.sprite(400, 300, 'redButton01').setInteractive();
+        this.centerButton(this.nextButton, -2);
+ 
+        this.backText = this.add.text(0, 0, 'PLAY', { fontFamily: 'Berlin Sans FB, "Goudy Bookletter 1911", Times, serif', fontSize: '32px', fill: '#fff' });
+        this.centerButtonText(this.backText, this.nextButton);
+ 
+        this.nextButton.on('pointerdown', function (pointer) {
+            this.selectAudio.stop();
+            this.scene.restart('DemoScene');
+            this.scene.start('DemoScene', {eleccion1: this.eleccion1, eleccion2: this.eleccion2});
+        }.bind(this));
+ 
+        this.input.on('pointerover', () => this.nextButton.setTexture('redButton02'));
+ 
+        this.input.on('pointerout', () => this.nextButton.setTexture('redButton01'));
+    }
+
+    updateAudio() {
+        if (musicOn === false) {
+          
+            this.selectAudio.stop();
+
+        } 
+        else 
+        {
+
+            this.selectAudio.play();
+        }
     }
 
     
